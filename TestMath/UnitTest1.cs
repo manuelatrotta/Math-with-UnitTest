3046c4a79a6326011e3a58f2240ec3034fa97b25
Manuela Trotta <MTrotta@qubicaamf.com>
2021-07-13 14:33:04 +0200

    enter TestCase- refactor test for method Max

--- TestMath/UnitTest1.cs
+++ TestMath/UnitTest1.cs
@@ -4,11 +4,12 @@ namespace TestMath
 {
     [TestFixture]
     public class MathTests
-    {   
+    {
         // aggiunta classe Math private e metodo setup con attibuto Setup per pulizia codice della ripetuta creazione d'istanza Math nei casi di test
         private Math _math;
 
-        public void Setup()
+        [SetUp]
+        public void SetUp()
         {
             _math = new Math();
         }
@@ -24,37 +25,40 @@ namespace TestMath
             Assert.That(result, Is.EqualTo(3));
 
         }
-
+        // utilizzando i TestCase è possibile inserire i diversi casi tramite i parametri e il valore aspettato riducendo il codice di test in un solo metodo
         [Test]
-        public void Max_FirstArgumentGreater_ReturnFirst()
+        [TestCase(1,2,2)]
+        [TestCase(2, 1, 2)]
+        [TestCase(1, 1, 1)]
+        public void Max_WhenCalled_ReturnGreaterThanArgument(int a, int b, int expectedResult)
         {
             //var math = new Math();
 
-            var result = _math.Max(2, 1);
+            var result = _math.Max(a, b);
 
-            Assert.That(result, Is.EqualTo(2));
+            Assert.That(result, Is.EqualTo(expectedResult));
 
-           
-        }
-        [Test]
-        public void Max_SecondtArgumentGreater_ReturnSecond()
-        {
+
+       }
+     //   [Test]
+     //   public void Max_SecondtArgumentGreater_ReturnSecond()
+      //  {
             //var math = new Math();
 
-            var result = _math.Max(1, 2);
+       //     var result = _math.Max(1, 2);
 
-            Assert.That(result, Is.EqualTo(2));
+       //     Assert.That(result, Is.EqualTo(2));
 
-        }
-        [Test]
-        public void Max_FirstAreEqualSecondArgumentGreater_ReturnAreEqual()
-        {
+      //  }
+       // [Test]
+      //  public void Max_FirstAreEqualSecondArgumentGreater_ReturnAreEqual()
+      //  {
             //var math = new Math();
 
-            var result = _math.Max(2, 2);
+       //     var result = _math.Max(2, 2);
 
-            Assert.That(result, Is.EqualTo(2));
-        }
+       //     Assert.That(result, Is.EqualTo(2));
+       // }
 
 
     }
